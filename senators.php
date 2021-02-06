8f78013a5b4045d7bf7a1559ace4ce3d6d4807a9
Esanul Kahn <esanul.kahn@automationanywhere.com>
2021-02-05 21:28:36 -0500

    Refactor -- address made into sequential array so that JSON parses it as an array (associative arrays treated as objects -- tricky, tricky ;-) )

--- senators.php
+++ senators.php
@@ -32,18 +32,20 @@ Class Senator{
             foreach ($member_array as $new_member){
                 $exploded_address_array = explode(' ',  $new_member['address']);
                 $street = $exploded_address_array[0]. " " .$exploded_address_array[1]. " " . $exploded_address_array[2]. " " . $exploded_address_array[3]. " " . $exploded_address_array[4]. " " ;
+                $address_array = [];
+                $address_array[] = [
+                    'street' => $street,
+                    'city' => $exploded_address_array[5],
+                    'state' => $exploded_address_array[6],
+                    'postal' => $exploded_address_array[7]
+                ];
                 $new_member_array[] = [
                     'firstName' => $new_member['first_name'],
                     'lastName'  => $new_member['last_name'],
                     'fullName'  => $new_member['first_name'] . " " . $new_member['last_name'],
                     'chartId'   => $new_member['bioguide_id'],
                     'mobile'    => $new_member['phone'],
-                    'address'   => [
-                        'street' => $street,
-                        'city' => $exploded_address_array[5],
-                        'state' => $exploded_address_array[6],
-                        'zip' => $exploded_address_array[7],
-                    ]
+                    'address'   => $address_array
                 ];
             }
             print_r(json_encode($new_member_array));
