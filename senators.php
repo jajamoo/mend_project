bdbb915ed67cb85b01d0f9346508debb152eb13c
Esanul Kahn <esanul.kahn@automationanywhere.com>
2021-02-05 20:34:46 -0500

    refactoring

--- senators.php
+++ senators.php
@@ -7,43 +7,50 @@ Class Senator{
 
     private $client;
 
+    /**
+     * Senator constructor.
+     */
     public function __construct()
     {
         $this->client = new Client();
     }
 
+    /**
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
     public function getSenators()
     {
-        $response = $this->client->get('https://www.senate.gov/general/contact_information/senators_cfm.xml');
-        $xml_string = simplexml_load_string((string) $response->getBody());
-        $member_array = [];
+        try {
+            $response = $this->client->get('https://www.senate.gov/general/contact_information/senators_cfm.xml');
+            $xml_string = simplexml_load_string((string) $response->getBody());
+            $member_array = [];
 
-        foreach ($xml_string->member as $member) {
-            $member_array[] = (array) $member;
+            foreach ($xml_string->member as $member) {
+                $member_array[] = (array) $member;
+            }
+
+            foreach ($member_array as $new_member){
+                $exploded_address_array = explode(' ',  $new_member['address']);
+                $street = $exploded_address_array[0]. " " .$exploded_address_array[1]. " " . $exploded_address_array[2]. " " . $exploded_address_array[3]. " " . $exploded_address_array[4]. " " ;
+                $new_member_array[] = [
+                    'firstName' => $new_member['first_name'],
+                    'lastName'  => $new_member['last_name'],
+                    'fullName'  => $new_member['first_name'] . " " . $new_member['last_name'],
+                    'chartId'   => $new_member['bioguide_id'],
+                    'mobile'    => $new_member['phone'],
+                    'address'   => [
+                        'street' => $street,
+                        'city' => $exploded_address_array[5],
+                        'state' => $exploded_address_array[6],
+                        'zip' => $exploded_address_array[7],
+                    ]
+                ];
+            }
+            print_r(json_encode($new_member_array));
+        } catch (\GuzzleHttp\Exception\GuzzleException $e) {
+            echo $e->getMessage();
         }
-
-        foreach ($member_array as $new_member){
-            $exploded_address_array = explode(' ', (string) $new_member['address']);
-            $street = $exploded_address_array[0]. " " .$exploded_address_array[1]. " " . $exploded_address_array[2]. " " . $exploded_address_array[3]. " " . $exploded_address_array[4]. " " ;
-            $new_member_array[] = [
-                'firstName' => (string) $new_member['first_name'],
-                'lastName' => (string) $new_member['last_name'],
-                'fullName' => (string) $new_member['first_name'] . " " . $new_member['last_name'],
-                'chartId' => (string) $new_member['bioguide_id'],
-                'mobile' => (string) $new_member['phone'],
-                'address' => [
-                    'street' => $street,
-                    'city' => $exploded_address_array[5],
-                    'state' => $exploded_address_array[6],
-                    'zip' => $exploded_address_array[7],
-                ]
-            ];
-        }
-
-        print_r(json_encode($new_member_array));
     }
 }
 
 (new Senator())->getSenators();
-
-//print_r(json_encode($new_member_array));
